69e6ff90c662c7e24beae5199d6546dc6a14d0e1
Carolina <carolgarcia@dit.upm.es>
2012-04-26 16:00:03 +0200

    Improve places list view

--- app/helpers/placelikes_helper.rb
+++ app/helpers/placelikes_helper.rb
@@ -1,9 +1,9 @@
 module PlacelikesHelper
 	# Construct the like sentence below the name of a Place
-	def like_place_sentence(activity)
-		friends_count = friends_like(activity)
+	def like_place_sentence(place)
+		friends_count = friends_like(place)
 		#likes_count = activity.direct_object.like_count
-		likes_count = activity.siblings.joins(:activity_verb).where('activity_verbs.name' => "like").count
+		likes_count = place.post_activity.children.joins(:activity_verb).where('activity_verbs.name' => "like").count
 		others_count = likes_count - friends_count
 		like_sentence = ""
 
@@ -33,14 +33,16 @@ module PlacelikesHelper
 	end
 
 	# Return the number of friends who like this activity
-	def friends_like(activity)
+	def friends_like(place)
 		# Total number of likes of the place, including me
-		likes = activity.siblings.joins(:activity_verb).where('activity_verbs.name' => "like")
+		likes = place.post_activity.children.joins(:activity_verb).where('activity_verbs.name' => "like")
 		friend_likes = likes.joins(:channel).joins('INNER JOIN contacts ON contacts.receiver_id = channels.author_id').
 			where('contacts.sender_id' => current_subject).where('contacts.ties_count' => 1)
 
 		friend_likes.count
 	end
+
+
 
 	def place_link_like(object)
 	    params = place_link_like_params(object)
@@ -62,6 +64,30 @@ module PlacelikesHelper
 	        params << image_tag("btn/nolike.png", :class => "menu_icon")+t('activity.verb.like.Place.like')
 	        params << [object, :like]
 	        params << {:class => "verb_like",:id => "like_" + dom_id(object),:method => :post, :remote => true}
+	      end
+	    end
+	end
+
+	def place_star(object)
+		params = place_star_params(object)
+		link_to params[0], params[1], params [2]
+	end
+
+	 def place_star_params(object)
+	    params = Array.new
+	    if !user_signed_in?
+	      params << image_tag("nolike-22x22.png", :class => "place_star")
+	      params << new_user_session_path
+	      params << {:class => "place_list_star",:id => "like_" + dom_id(object), :title => t('activity.verb.like.Place.like')}
+	    else
+	      if (object.liked_by?(current_subject))
+	        params << image_tag("like-22x22.png", :class => "place_star")
+	        params << [object, :like]
+	        params << {:class => "place_list_star",:id => "like_" + dom_id(object),:method => :delete, :remote => true, :title => t('activity.verb.like.Place.unlike')}
+	      else
+	        params << image_tag("nolike-22x22.png", :class => "place_star")
+	        params << [object, :like]
+	        params << {:class => "place_list_star",:id => "like_" + dom_id(object),:method => :post, :remote => true, :title => t('activity.verb.like.Place.like')}
 	      end
 	    end
 	end
