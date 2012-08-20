c81e38d9791f001b4444ec3424ae6d8c055533b0
Carolina Garcia <carolgarcia@dit.upm.es>
2012-08-20 16:47:40 +0200

    Try heroku with mysql2

--- config/application.rb
+++ config/application.rb
@@ -80,5 +80,8 @@ module Places
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Initialize app and connect to DB while precompiling assets
+    config.assets.initialize_on_precompile = false
   end
 end
