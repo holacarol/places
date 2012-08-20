d50fa08a4ddc4ecf2d61076147d4dc29c69e541b
Carolina Garcia <carolgarcia@dit.upm.es>
2012-08-20 15:54:37 +0200

    Try to fix PostgreSQL bug

---
+++ config/initializers/postgresqladapter_patch.rb
@@ -0,0 +1,8 @@
+# PostgreSQLAdapter patch
+
+
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+  def tables(name = nil)
+    query("SELECT tablename FROM pg_tables WHERE schemaname = ANY (current_schemas(false))", name).map{|row| row[0]}
+  end
+end
