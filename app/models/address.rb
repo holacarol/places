d685072c9da06db1d9bfbfc0ed1eacf9b93809f5
Carolina <carolgarcia@dit.upm.es>
2013-01-18 16:37:46 +0100

    Change postalcode requirement and add mail support

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,7 +7,7 @@ class Address < ActiveRecord::Base
   validates :streetAddress, :presence => true
   validates :locality, :presence => true
   #validates :region, :presence => true
-  validates :postalCode, :presence => true
+  #validates :postalCode, :presence => true
   validates :country, :presence => true
   #validates_uniqueness_of :streetAddress, :scope => [:locality, :postalCode]
 
